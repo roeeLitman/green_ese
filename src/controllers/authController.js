50896291e4b58acda8f7ee586e5b4214d49dd0d0
roee ltmanovich <roeeli770@gmail.com>
2024-10-07 18:08:18 +0300

    create register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,22 @@
-const login =  () => {
+const { UserModel } = require('../models/userModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+
+const login =  async(user) => {
     try {
-        
+        const dbUser = await UserModel.findOne({user_name: user.user_name }) 
+
+        if (!dbUser) throw new Error('user not found')
+
+        if (!(await bcrypt.compare(user.password, dbUser.password))){
+            throw new Error('password worning')
+        }
+        const token = await jwt.sign({
+            user_name,
+            role: dbUser.role
+        }, process.env.TOKEN_SECRET)
+
     } catch (err) {
         
     }
