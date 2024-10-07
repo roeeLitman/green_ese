64fb38300091959e764d6485e0b1e50ce08a15a7
roee ltmanovich <roeeli770@gmail.com>
2024-10-07 18:12:17 +0300

    Connecting wires

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,31 @@
-const register =  () => {}
-const getProfile =  () => {}
-const setSettings =  () => {}
+const {creteUser} = require('../services/userService')
+
+const register = async (req, res) => {
+    try {
+         await creteUser(req.body)
+         res.status(200).json({
+            mge: "user created"
+         })
+    } catch (err) {
+        res.status(400).json({err:err.message})
+    }
+}
+
+const getProfile =  () => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+const setSettings =  () => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
 
 module.exports = {
     register,
