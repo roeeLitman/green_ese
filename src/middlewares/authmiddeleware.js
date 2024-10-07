d1da85b9af3785c312f74c4d0bdba0fef7088eed
roee ltmanovich <roeeli770@gmail.com>
2024-10-08 01:04:39 +0300

    create 2 functions middeleware

--- src/middlewares/authmiddeleware.js
+++ src/middlewares/authmiddeleware.js
@@ -1,8 +1,40 @@
+const jwt = require("jsonwebtoken")
+
 const onlyCommanders = async(req, res, next) => {
+    try {
+        //chek cooki
+        const token = req.cookies.token
+        //translit cooki
+        const userData = jwt.verify(token, process.env.TOKEN_SECRET)
+        //chek if role is commender
+        if (userData.role !== "commander") {
+            res.status(403).send("You not have access ")
+          }
+        //insert into res.user
+        req.user = userData
+        // call next
+        next()
+    } catch (err) {
+        console.log(err);
+        res.status(401).json({mge: 'not founf token'})  
+    }
 
 }
 
 const onlySoldiersAndCommanders  = async(req, res, next) => {
+    try {
+        //chek cooki
+        const token = req.cookies.token
+        //translit cooki
+        const userData = jwt.verify(token, process.env.TOKEN_SECRET)
+        //insert into res.user
+        req.user = userData
+        // call next
+        next()
+    } catch (err) {
+        console.log(err);
+        res.status(401).json({mge: 'not founf token'})  
+    }
 
 }
 
