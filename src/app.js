44935d014751fca7829f005b82fbce29d9aa544f
roee ltmanovich <roeeli770@gmail.com>
2024-10-08 00:41:43 +0300

    add cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const exp = require('express')
 const dotenv = require('dotenv')
 const {connectionToMongo} = require('./config/dbConfig')
+const cookieParser = require('cookie-parser')
 
 
+// גישה למשתני סביבה
 dotenv.config ()
+// פןרט
 const port = process.env.PORT || 1415
+//חיבור לבסיס נתונים
 connectionToMongo()
 
 const app = exp()
 app.use(exp.json())
+app.use(cookieParser())
+
 app.use('/auth', require('./routes/authRouter'))
 app.use('/uesr', require('./routes/userRouter'))
 app.use('/greenEye', require('./routes/greenEyeRouter'))
