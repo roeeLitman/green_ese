2354344cc85db45645a056de2db85438c2207f36
roee ltmanovich <roeeli770@gmail.com>
2024-10-08 12:26:02 +0300

    save

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,35 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchma = new mongoose.Schema({
-    user_name: {
+  user_name: {
     type: String,
-    require:[true, 'user name is require'],
-    },
-    password: {
-        type:String,
-        required:[true,"worning password"]
-    },
-    role:{
-        type: String,
-        enum:['soldier', 'commander'],
-        required:[true, 'you must to giv rool']
+    require: [true, "user name is require"],
+  },
+  password: {
+    type: String,
+    required: [true, "worning password"],
+  },
+  role: {
+    type: String,
+    enum: ["soldier", "commander"],
+    required: [true, "you must to giv rool"],
+  },
+  area: {
+    type: String,
+    enum: ["center", "north", " south", "west", " east"],
+    required: [true, "please provide area"],
+  },
+  units: {
+    type: [Number],
+    required: [true, "please specify at least one unit"],
+  },
+});
 
-
-    },
-    area: {
-        type: String,
-        enum:['center', 'north', ' south', 'west', ' east'],
-        required:[true, "please provide area"]
-    },
-    units: {
-        type:[Number],
-        required:[true, 'please specify at least one unit']
-    }
-})
-
-const UserModel = mongoose.model('user', userSchma)
+const UserModel = mongoose.model("user", userSchma);
 
 module.exports = {
-    UserModel,
-    userSchma
-}
+  UserModel,
+  userSchma,
+};
